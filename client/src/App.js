d72fe38199042c89f6994f555f6d15102b2cdf1d
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-05-17 10:26:10 +0300

    feature: Setup nginx reverse proxy with docker, express and react.

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = (props) => {
+  return (
+   <div>
+   <h1>
+       Welcome to the mern invoice app !!
+   </h1>
+   
+   </div>
+ );}
+
+export default App;
