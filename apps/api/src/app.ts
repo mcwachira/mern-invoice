e52004360efa5ab33169771ff462eccb1ec8064c
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-06-11 14:42:09 +0300

    update my docker file and app.ts

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -2,9 +2,10 @@ import chalk from "chalk";
 import cookieParser from "cookie-parser";
 import express, { Request, Response } from "express";
 import morgan from "morgan";
+import { morganMiddleware, systemLogs } from "./utils/logger";
 
 const app = express();
-const port = process.env.PORT || 8000;
+const PORT = process.env.PORT || 1997;
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -14,13 +15,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
+app.use(morganMiddleware);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello from Express with TypeScript!");
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(
-    `${chalk.green.bold(`Server running at http://localhost:${port}`)}`,
+    `${chalk.green.bold("Server  running in ")} ${chalk.yellow.bold(process.env.NODE_ENV)} mode on port  ${chalk.blue.bold(PORT)}`,
+  );
+  systemLogs.info(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`,
   );
 });
