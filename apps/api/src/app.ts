ce40f854e8bb2ba6ed978ebfe29cb59a443ae5d8
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-06-11 11:01:19 +0300

    initialized the monorepo using turob repo and setup my backend api using typescript and express

---
+++ apps/api/src/app.ts
@@ -0,0 +1,23 @@
+import cookieParser from "cookie-parser";
+import express, { Request, Response } from "express";
+import morgan from "morgan";
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from Express with TypeScript!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
