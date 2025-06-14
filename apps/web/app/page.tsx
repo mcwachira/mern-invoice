0a7cfeb4a7332321a40984c5147ac04abb31268f
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-06-14 16:01:09 +0300

    Added the homepage  its styling and the images needed

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,33 @@
-export default function Home() {
+"use client";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import "./homepage.css";
+
+const Home = () => {
+  const router = useRouter();
   return (
-    <div>
-      a<h1>Hello steve</h1>
-    </div>
+    <header className="bg-cover bg-center h-screen main-bg-image flex flex-col justify-center items-center px-4 text-center">
+      <h1 className="text-[8rem] sm:text-[12rem] font-extrabold uppercase text-white leading-none">
+        MERN Invoice
+      </h1>
+
+      <p className="text-white text-opacity-70 text-xl max-w-3xl mt-4">
+        Whatever business you run, creating invoices, receipts, and quotations
+        is made easy with our app.
+      </p>
+
+      <div className="mt-10">
+        <Button
+          variant="default"
+          size="lg"
+          className="text-2xl px-8 py-4 rounded-full bg-green-600 hover:bg-green-700"
+          onClick={() => router.push("/register")}
+        >
+          Create Account
+        </Button>
+      </div>
+    </header>
   );
-}
+};
+
+export default Home;
