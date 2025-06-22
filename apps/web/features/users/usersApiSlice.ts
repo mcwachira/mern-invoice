76c1c3cb4edc0c8f3759c4436dfda169bfc3751c
mcwachira <mcwachira@outlook.com>
2025-06-22 14:15:28 +0300

    Added more api slices for users

--- apps/web/features/users/usersApiSlice.ts
+++ apps/web/features/users/usersApiSlice.ts
@@ -21,7 +21,50 @@ export const usersApiSlice = baseApiSlice.injectEndpoints({
             ]
           : [{ type: "User", id: "LIST" }],
     }),
+
+    getUserProfile: builder.query({
+      query: () => "/user/profile",
+      providesTags: [{ type: "User", id: "SINGLE_USER" }],
+    }),
+    updateUserProfile: builder.mutation({
+      query: (profileData) => ({
+        url: "/user/profile",
+        method: "PATCH",
+        body: profileData,
+      }),
+      invalidatesTags: [{ type: "User", id: "SINGLE_USER" }],
+    }),
+
+    deleteMyAccount: builder.mutation({
+      query: () => ({
+        url: `/user/profile`,
+        method: "DELETE",
+      }),
+      invalidatesTags: [{ type: "User", id: "LIST" }],
+    }),
+
+    deleteUser: builder.mutation({
+      query: (id) => ({
+        url: "/user/${id}",
+        method: "DELETE",
+      }),
+      invalidatesTags: [{ type: "User", id: "LIST" }],
+    }),
+    deactivateUser: builder.mutation({
+      query: (id) => ({
+        url: `/user/${id}/deactivate`,
+        method: "PATCH",
+      }),
+      invalidatesTags: [{ type: "User", id: "LIST" }],
+    }),
   }),
 });
 
-export const { useGetAllUsersQuery } = usersApiSlice;
+export const {
+  useGetAllUsersQuery,
+  useDeleteUserMutation,
+  useDeleteMyAccountMutation,
+  useDeactivateUserMutation,
+  useGetUserProfileQuery,
+  useUpdateUserProfileMutation,
+} = usersApiSlice;
